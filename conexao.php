c8257e0c0a6fcf816b523825f92fa7ec08a2fb9b
gcbruna <bruhbg55@gmail.com>
2024-05-23 10:36:48 -0300

    criando uma pasta para os styles

---
+++ conexao.php
@@ -0,0 +1,13 @@
+<?php
+$dsn = 'mysql:host=localhost;dbname=registerpet';
+$user = 'root'; 
+$password = ''; 
+
+try {
+    $dbh = new PDO($dsn, $user, $password);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+    exit; // interrompe a execução do script em caso de falha na conexão
+}
+?>

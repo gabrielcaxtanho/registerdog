261d60245c5ed6fd909546353350c95cac8b2e1f
gcbruna <bruhbg55@gmail.com>
2024-08-14 18:22:32 -0300

    *add* arquivo para reutilização

---
+++ php/footer_index.php
@@ -0,0 +1,14 @@
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<footer class="p-3 mb-2 bg-dark-subtle text-dark-emphasis">
+    <div class="d-flex flex-column align-items-center">
+        <i class="fa-solid fa-paw text-danger-emphasis" style="font-size: 5em; padding:0.5em"></i>
+        <p>Um pet bem-cuidado é um pet feliz, que retribui todo o amor e atenção com lealdade e carinho</p>
+    </div>
+    <p class="text-center p-2"><i class="fa-solid fa-copyright"></i> Sistemas G&B 2020 - 2024</p>
+    <p class="text-center p-2">Versão 1.0</p>
+
+</footer>
+<script src="https://kit.fontawesome.com/c0eae24639.js" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>

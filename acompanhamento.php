7cf383be5872b6445ef933dbe6097fc993b5d895
Gabriel Chavezz <96641560+gabrielcaxtanho@users.noreply.github.com>
2024-08-20 13:36:38 -0300

    Create acompanhamento.php
    pagina nova

---
+++ acompanhamento.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Editar Pet</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/petcadastrado.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <?php include_once 'php/header_index.php'; ?>
+
+    <div class="container my-5">
+        <div class="row justify-content-center">
+
+            <div class="text-center" style="padding-bottom: 40px; display: flex; flex-direction: row; justify-content: center; align-items: center; gap: 5px;">
+                <h4>Acompanhe aqui o andamento do seu Pet<h4><i class="fa-solid fa-cat"></i>
+            </div>
+
+            <div class="col-md-15">
+                <!-- Primeiro Card -->
+                <div class="mb-4 " >
+                    <div class="card-body text-center">
+                    <div class="card shadow rounded p-4 h-100" style="border-top: #D3D04F 7px solid;">
+                        <div class="d-flex justify-content-center align-items-center">
+                            <h5 class="card-title mb-0">BANHO</h5>
+                            <i class="fa-solid fa-shower ms-2"></i>
+                        </div>
+                            <p class="card-text m-2">STATUS: CONCLUIDO</p>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Segundo Card -->
+                <div class="mb-4 " >
+                    <div class="card-body text-center">
+                    <div class="card shadow rounded p-4 h-100" style="border-top: #8EAC50 7px solid;">
+                        <div class="d-flex justify-content-center align-items-center">
+                            <h5 class="card-title mb-0">TOSA</h5>
+                            <i class="fa-solid fa-scissors ms-2"></i>
+                        </div>
+                            <p class="card-text m-2">STATUS: EM ANDAMENTO</p>
+                        </div>
+                    </div>
+                </div>
+                
+
+                <!-- Terceiro Card -->
+                <div class="mb-4 " >
+                    <div class="card-body text-center">
+                    <div class="card shadow rounded p-4 h-100" style="border-top: #17594A 7px solid;">
+                        <div class="d-flex justify-content-center align-items-center">
+                            <h5 class="card-title mb-0">SECAGEM</h5>
+                            <i class="fa-solid fa-temperature-three-quarters ms-2"></i>
+                        </div>
+                            <p class="card-text m-2">STATUS: AGUARDANDO CONCLUSAO DE OUTRAS ETAPAS</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php include_once 'php/footer_index.php'; ?>
+    
+    <script src="https://kit.fontawesome.com/c0eae24639.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>

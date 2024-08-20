1052e67f9bab2ff3b313eca57ff0b5571469a52d
gcbruna <bruhbg55@gmail.com>
2024-08-20 18:12:09 -0300

    add header e footer

--- cadastro.php
+++ cadastro.php
@@ -87,80 +87,89 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro de Pets</title>
-    <link rel="stylesheet" href="css/cadastro_style.css">
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 
 <body>
-    <main class="rodape">
-        <h1 class="rodape__texto"><i class="fa-solid fa-paw fa-2xl" style="color: #ffffff; margin: 22px;"></i>PET REGISTER</h1>
-        <h3 class="rodape__texto">TODOS SEUS REGISTROS EM UM SÓ LUGAR</h3>
-    </main>
+    <?php include_once 'php/header_index.php'; ?>
+    <!-- <main class="font">
+        <h1 class="font__texto"><i class="fa-solid fa-paw fa-2xl" style="color: #ffffff; margin: 22px;"></i>PET REGISTER</h1>
+        <h3 class="font__texto">TODOS SEUS REGISTROS EM UM SÓ LUGAR</h3>
+    </main> -->
     <?php if ($mensagem) : ?>
         <section class="mensagem">
             <?= $mensagem ?>
         </section>
     <?php endif; ?>
 
-    <section class="principal">
-        <section class="principal__imagemfundo">
-            <div class="principal__elemento">
-                <section class="formulario_central">
-                    <h1 id="titulo-cadastro">cadastro pet</h1>
-                    <form action="" method="post" class="formulario">
-                        <div class="elemento_formulario">
-                            <label for="raca">raça<b style="color: #ff0000;">*</b></label>
-                            <input class="campo" type="text" id="raca" name="raca" required>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-10">
+                <h2 class="text-center" id="font"> cadastrar pet </h2>
+                <hr class="border-white">
+                <div class="shadow rounded p-4 mb-4" style="border-top: #2e8a97 7px solid;">
+                    <form id="editForm" method="post" action=".php">
+                        <div class="mb-3">
+                            <label for="raca" class="form-label"><strong>raça </strong><b style="color: #ff0000;">*</b></label>
+                            <input class="form-control" type="text" id="raca" name="raca" required>
                         </div>
-                        <div class="elemento_formulario">
-                            <label for="nomePet">nome do pet<b style="color: #ff0000;">*</b></label>
-                            <input class="campo" type="text" id="nomePet" name="nomePet" required>
+                        <div class="mb-3">
+                            <label for="nomePet" class="form-label"><strong>nome do pet </strong><b style="color: #ff0000;">*</b></label>
+                            <input class="form-control" type="text" id="nomePet" name="nomePet" required>
                         </div>
-                        <div class="elemento_formulario">
-                            <label for="nomeTutor">nome do tutor<b style="color: #ff0000;">*</b></label>
-                            <input class="campo" type="text" id="nomeTutor" name="nomeTutor" required>
+                        <div class="mb-3">
+                            <label for="nomeTutor" class="form-label"><strong>nome to tutor </strong><b style="color: #ff0000;">*</b></label>
+                            <input class="form-control" type="text" id="nomeTutor" name="nomeTutor" required>
                         </div>
-                        <div class="elemento_formulario">
-                            <label for="contatoTutor">contato do tutor<b style="color: #ff0000;">*</b></label>
-                            <input class="campo" type="text" id="contatoTutor" name="contatoTutor" required>
+                        <div class="mb-3">
+                            <label for="contatoTutor" class="form-label"><strong>contato </strong><b style="color: #ff0000;">*</b></label>
+                            <input class="form-control" type="text" id="contatoTutor" name="contatoTutor" required>
                         </div>
-                        <div class="elemento_formulario">
-                            <label>serviços<b style="color: #ff0000;">*</b></label>
-                            <div class="elemento_formulario_2">
-                                <input type="radio" name="descricao" value="Banho" required>
-                                <label for="banho">banho</label>
+                        <div class="mb-3">
+                            <label class="form-label"><strong>serviços </strong><b style="color: #ff0000;">*</b></label>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="descricao" value="Banho" required>
+                                <label class="form-check-label" for="banho">banho</label>
                             </div>
-                            <div class="elemento_formulario_2">
-                                <input type="radio" name="descricao" value="Tosa">
-                                <label for="tosa">tosa</label>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="descricao" value="Tosa">
+                                <label class="form-check-label" for="tosa">Tosa</label>
                             </div>
-                            <div class="elemento_formulario_2">
-                                <input type="radio" name="descricao" value="Banho e Tosa">
-                                <label for="banhoTosa">banho e tosa</label>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="descricao" value="Banho e Tosa">
+                                <label class="form-check-label" for="banhoTosa">Banho e Tosa</label>
                             </div>
-                            <div class="elemento_formulario_2">
-                                <input type="radio" name="descricao" value="Vacinação">
-                                <label for="vacinacao">vacinação</label>
+                           <!--  <div class="form-check">
+                                <input class="form-check-input" type="radio" name="descricao" value="Vacinação">
+                                <label class="form-check-label" for="vacinacao">Vacinação</label>
                             </div>
-                            <div class="elemento_formulario_2">
-                                <input type="radio" name="descricao" value="Consulta Veterinária">
-                                <label for="consultaVeterinaria">consulta veterinária</label>
-                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="descricao" value="Consulta Veterinária">
+                                <label class="form-check-label" for="consultaVeterinaria">Consulta Veterinária</label>
+                            </div> -->
                         </div>
-                        <div class="elemento_formulario">
-                            <label for="observacao">observações</label>
-                            <input class="campo" type="text" id="observacao" name="observacao">
+                        <div class="mb-3">
+                            <label for="observacao" class="form-label"><strong>observações </strong></label>
+                            <input class="form-control" type="text" id="observacao" name="observacao">
                         </div>
-                        <div class="elemento_formulario_botoes">
-                            <button type="submit" class="button-19" role="button">CADASTRAR PET</button>
-                            <button type="button" class="button-19_2" role="button" onclick="window.location.href='./index.php'">CANCELAR</button>
+                        <div class="card-footer bg-transparent text-center">
+                            <button type="submit" class="btn btn-primary w-100 mb-3">Cadastrar Pet</button>
+                            <a href="./index.php" class="text-danger text-decoration-underline">Cancelar</a>
                         </div>
                     </form>
-                </section>
+                </div>
             </div>
-        </section>
+        </div>
+    </div>
+        </div>
+    </div>
 
     </section>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/c0eae24639.js" crossorigin="anonymous"></script>
+    <?php
+    include_once 'php/footer_index.php';
+    ?>
 </body>
+
 </html>
